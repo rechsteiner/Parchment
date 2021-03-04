499e9e488c0cc936ee2919dcbae4a410a3419622
Alejandro Carretero García <alejandrocgf@me.com>
2021-03-04 12:16:29 +0100

    Fix menu height
    Fix menu height in PagingView
    
    Signed-off-by: Alejandro Carretero García <alejandrocgf@me.com>

--- Parchment/Classes/PagingView.swift
+++ Parchment/Classes/PagingView.swift
@@ -8,24 +8,24 @@ import UIKit
 /// constraints, you can subclass `PagingView` and override
 /// `loadView:` in `PagingViewController` to use your subclass.
 open class PagingView: UIView {
-  
+
   // MARK: Public Properties
-  
+
   public let collectionView: UICollectionView
   public let pageView: UIView
   public var options: PagingOptions {
     didSet {
-      heightConstraint?.constant = options.menuItemSize.height
+      heightConstraint?.constant = options.menuHeight
       collectionView.backgroundColor = options.menuBackgroundColor
     }
   }
-  
+
   // MARK: Private Properties
-  
+
   private var heightConstraint: NSLayoutConstraint?
-  
+
   // MARK: Initializers
-  
+
   /// Creates an instance of `PagingView`.
   ///
   /// - Parameter options: The `PagingOptions` passed into the
@@ -36,13 +36,13 @@ open class PagingView: UIView {
     self.pageView = pageView
     super.init(frame: .zero)
   }
-  
+
   required public init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   // MARK: Public Methods
-  
+
   /// Configures the view hierarchy, sets up the layout constraints
   /// and does any other customization based on the `PagingOptions`.
   /// Override this if you need any custom behavior.
@@ -52,38 +52,38 @@ open class PagingView: UIView {
     addSubview(collectionView)
     setupConstraints()
   }
-  
+
   /// Sets up all the layout constraints. Override this if you need to
   /// make changes to how the views are layed out.
   open func setupConstraints() {
     collectionView.translatesAutoresizingMaskIntoConstraints = false
     pageView.translatesAutoresizingMaskIntoConstraints = false
-    
+
     let metrics = [
       "height": options.menuHeight]
-    
+
     let views = [
       "collectionView": collectionView,
       "pageView": pageView]
-    
+
     #if swift(>=4.2)
     let formatOptions = NSLayoutConstraint.FormatOptions()
     #else
     let formatOptions = NSLayoutFormatOptions()
     #endif
-    
+
     let horizontalCollectionViewContraints = NSLayoutConstraint.constraints(
       withVisualFormat: "H:|[collectionView]|",
       options: formatOptions,
       metrics: metrics,
       views: views)
-    
+
     let horizontalPagingContentViewContraints = NSLayoutConstraint.constraints(
       withVisualFormat: "H:|[pageView]|",
       options: formatOptions,
       metrics: metrics,
       views: views)
-    
+
     let verticalConstraintsFormat: String
     switch options.menuPosition {
     case .top:
@@ -97,16 +97,16 @@ open class PagingView: UIView {
       options: formatOptions,
       metrics: metrics,
       views: views)
-    
+
     addConstraints(horizontalCollectionViewContraints)
     addConstraints(horizontalPagingContentViewContraints)
     addConstraints(verticalContraints)
-    
+
     for constraint in verticalContraints {
       if constraint.firstAttribute == NSLayoutConstraint.Attribute.height {
         heightConstraint = constraint
       }
     }
   }
-  
+
 }

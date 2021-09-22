fac1471c36f86de492228c9d73036a278a10f31c
Duong Hien Phu <phucdth12a@gmail.com>
2021-09-22 17:23:55 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Parchment",
-    platforms: [.iOS("9.0")],
+        platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
     products: [
         .library(name: "Parchment", targets: ["Parchment"]),
     ],
